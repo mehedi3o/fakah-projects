6c589391d7b6496b4553140b691ad892cbd2f41f
mehedip1 <Promehedi30@gmail.com>
2024-11-26 11:28:08 +0600

    my new side bar add

--- script/script.js
+++ script/script.js
@@ -2,19 +2,30 @@ let loginForm = document.querySelector('.login-form');
 
 document.querySelector('#login-btn').onclick = () =>{
     loginForm.classList.toggle('active');
-    loginForm.classList.remove('active');
+    // loginForm.classList.remove('active');
 }
 
-let navbar = document.querySelector('.navbar');
+// let navbar = document.querySelector('.navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    loginForm.classList.remove('active');
+// document.querySelector('#menu-btn').onclick = () =>{
+//     navbar.classList.toggle('active');
+//     loginForm.classList.remove('active');
+// }
+
+// window.onscroll = () =>{
+//     loginForm.classList.remove('active');
+//     navbar.classList.remove('active');
+// }
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
 }
 
-window.onscroll = () =>{
-    loginForm.classList.remove('active');
-    navbar.classList.remove('active');
+function colsemenu(){
+    sidemenu.style.right = "-200px";
 }
+
 
 
