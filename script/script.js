43c651341e42cd17ece4cdb0333eed1156871ea7
mehedip1 <Promehedi30@gmail.com>
2024-11-27 00:21:55 +0600

    modify this code

--- script/script.js
+++ script/script.js
@@ -2,30 +2,23 @@ let loginForm = document.querySelector('.login-form');
 
 document.querySelector('#login-btn').onclick = () =>{
     loginForm.classList.toggle('active');
-    // loginForm.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
-// let navbar = document.querySelector('.navbar');
+let navbar = document.querySelector('.navbar');
 
-// document.querySelector('#menu-btn').onclick = () =>{
-//     navbar.classList.toggle('active');
-//     loginForm.classList.remove('active');
-// }
-
-// window.onscroll = () =>{
-//     loginForm.classList.remove('active');
-//     navbar.classList.remove('active');
-// }
-
-var sidemenu = document.getElementById("sidemenu");
-
-function openmenu(){
-    sidemenu.style.right = "0";
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    console.log('menu-btn');
+    loginForm.classList.remove('active');
 }
 
-function colsemenu(){
-    sidemenu.style.right = "-200px";
+
+window.onscroll = () =>{
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
 }
+
 
 
 
